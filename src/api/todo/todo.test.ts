cc2bf4035e5698af9bf400b756f838a0bfcade72
israelcena <israelsjm@gmail.com>
2022-09-10 14:55:49 -0300

    include drop todo beforeAll

--- src/api/todo/todo.test.ts
+++ src/api/todo/todo.test.ts
@@ -1,6 +1,15 @@
 import request from 'supertest';
-
 import app from '../../app';
+import { Todos } from './todo.model';
+
+beforeAll(async () => {
+  try {
+    await Todos.drop();
+  } catch (error) {
+    // console.error(error);
+  }
+  
+});
 
 describe('GET /api/v1/todo', () => {
   it('Responds with an array of todo', async () => 
@@ -11,8 +20,9 @@ describe('GET /api/v1/todo', () => {
       .expect(200)
       .then((response)=> {
         expect(response.body).toHaveProperty('length');
-        expect(response.body[0]).toHaveProperty('content');
-        expect(response.body[0]).toHaveProperty('done');
+        expect(response.body.length).toBe(0);
+        // expect(response.body[0]).toHaveProperty('content');
+        // expect(response.body[0]).toHaveProperty('done');
       }),
   );
 });
