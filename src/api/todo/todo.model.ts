e634bc8c32396d03dd8d3773d698e7870032441e
israelcena <israelsjm@gmail.com>
2022-09-10 14:06:55 -0300

    include get items in the db

--- src/api/todo/todo.model.ts
+++ src/api/todo/todo.model.ts
@@ -1,10 +1,12 @@
+import { db } from './../db';
 import { z } from 'zod';
+import { WithId } from 'mongodb';
 
 const Todo = z.object({
   content: z.string().min(1).max(64),
   done: z.boolean().default(false),
 });
 
-type Todo = z.infer<typeof Todo>;
-
-export default Todo;
+export type Todo = z.infer<typeof Todo>;
+export type TodoWithId = WithId<Todo>;
+export const Todos = db.collection<Todo>('todos');
--- src/api/todo/todo.routes.ts
+++ src/api/todo/todo.routes.ts
@@ -1,19 +1,12 @@
 import { Router, Request, Response } from 'express';
-import Todo from './todo.model';
+import { TodoWithId, Todos } from './todo.model';
 
 const router = Router();
 
-router.get('/', (req: Request, res: Response<Todo[]>) => {
-  res.json([{
-    content: 'Learn TypeScript',
-    done: false,
-  }, {
-    content: 'Estudar',
-    done: true,
-  }, {
-    content: 'Teste',
-    done: false,
-  }]);
+router.get('/', async (req: Request, res: Response<TodoWithId[]>) => {
+  const result = await Todos.find();
+  const todos = await result.toArray();
+  res.json(todos);
 });
 
 export default router;
