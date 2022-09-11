c9818657ef6b0e615026752ea19ea70014a57490
israelcena <israelsjm@gmail.com>
2022-09-11 04:44:37 -0300

    include try catch

--- src/api/todo/todo.handlers.ts
+++ src/api/todo/todo.handlers.ts
@@ -1,8 +1,13 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 import { TodoWithId, Todos } from './todo.model';
 
-export async function findAll(req: Request, res: Response<TodoWithId[]>) {
-  const result = await Todos.find();
-  const todos = await result.toArray();
-  res.json(todos);
+export async function findAll(req: Request, res: Response<TodoWithId[]>, next: NextFunction) {
+  try {
+    const result = await Todos.find();
+    const todos = await result.toArray();
+    res.json(todos);
+  } catch (error) {
+    next(error);
+  }
 }
+
